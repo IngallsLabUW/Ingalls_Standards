ffdf643c98c0ae04c4191b8fe81939a9217dbd24
Regina Lionheart <regina@lionhe.art>
2021-06-22 13:28:09 -0700

    Halfway commit for SQL tidy.

--- src/SQL_Safe.R
+++ src/SQL_Safe.R
@@ -1,42 +1,41 @@
 # Script for SQL-Safe, CMAP-Friendly compound names
-library(tidyverse)
 
 replace_special_characters <- function(x) (gsub("[^[:alnum:] ]", " ", x))
 replace_double_spaces <- function(x) (gsub("  ", " ", x))
 
-Special.Names <- read.csv("Ingalls_Lab_Standards_NEW.csv", stringsAsFactors = FALSE) %>%
+All_Standards <- Ingalls_Lab_Standards_ChEBI
+
+Special.Names <- Ingalls_Lab_Standards_ChEBI %>%
   select(Compound.Name, Compound.Name_figure) %>%
   filter_all(any_vars(str_detect(., "[^[:alnum:] ]"))) %>%
   unique() %>%
-  mutate(Fig.SpecialCharacter = ifelse(str_detect(Compound.Name_figure, "[^[:alnum:] ]"), TRUE, FALSE),
-         Long.SpecialCharacter = ifelse(str_detect(Compound.Name, "[^[:alnum:] ]"), TRUE, FALSE))
-
-Replace.Long <- Special.Names %>%
+  mutate(#Fig.SpecialCharacter = ifelse(str_detect(Compound.Name_figure, "[^[:alnum:] ]"), TRUE, FALSE),
+         Long.SpecialCharacter = ifelse(str_detect(Compound.Name, "[^[:alnum:] ]"), TRUE, FALSE)) %>%
   mutate(Compound.Name_SQL = ifelse(Long.SpecialCharacter == FALSE, Compound.Name, Compound.Name_figure)) %>%
   mutate_at(c("Compound.Name_SQL"), replace_special_characters) %>%
-  filter(Fig.SpecialCharacter == TRUE & Long.SpecialCharacter == TRUE)
-
-Replace.Specifics <- Replace.Long %>%
-  mutate(Compound.Name_SQL = recode(Compound.Name_SQL,
-                                    # Small fixes
-                                    "2 Hydroxy 4  methylthio butyric acid" = "2 Hydroxy 4 methylthio butyric acid",
-                                    "4 Hydroxybenzaldehyde" = "4 hydroxybenzaldehyde",
-                                    "7 DHC" = "7 Dehydrocholesterol",
-                                    "a Tocotrienol" = "alpha Tocotrienal",
-                                    "b Alanine" = "beta Alanine",
-                                    "b Carotene" = "beta Carotene",
-                                    "b Cyclocitral" = "beta Cyclocitral", 
-                                    "b Glutamic acid" = "beta Glutamic acid",
-                                    "b Ionine" = "beta Ionine",
-                                    "b Ionylidene acetaldehyde" = "beta Ionylidene acetaldehyde",
-                                    "cinnamoyl HSL" = "Cinnamoyl HSL",
-                                    "D SAM" = "Decarboxylated S Adenosylmethionine",
-                                    "F6 phosphate" = "Fructose 6 phosphate",
-                                    "Keto    methylthio butyric acid" = "Keto methylthiobutyric acid",
-                                    "N e  Acetyl Lysine" = "Ne Acetyl lysine")) %>%
   mutate_at(c("Compound.Name_SQL"), replace_double_spaces)
 
-LauraEditsSQL <- read.csv("data_raw/Standards_LeadingNumbers_LTC.csv") %>%
+# Replace.Specifics <- Special.Names %>%
+#   # mutate(Compound.Name_SQL = recode(Compound.Name_SQL,
+#   #                                   # Small fixes
+#   #                                   "2 Hydroxy 4  methylthio butyric acid" = "2 Hydroxy 4 methylthio butyric acid",
+#   #                                   "4 Hydroxybenzaldehyde" = "4 hydroxybenzaldehyde",
+#   #                                   "7 DHC" = "7 Dehydrocholesterol",
+#   #                                   "a Tocotrienol" = "alpha Tocotrienal",
+#   #                                   "b Alanine" = "beta Alanine",
+#   #                                   "b Carotene" = "beta Carotene",
+#   #                                   "b Cyclocitral" = "beta Cyclocitral", 
+#   #                                   "b Glutamic acid" = "beta Glutamic acid",
+#   #                                   "b Ionine" = "beta Ionine",
+#   #                                   "b Ionylidene acetaldehyde" = "beta Ionylidene acetaldehyde",
+#   #                                   "cinnamoyl HSL" = "Cinnamoyl HSL",
+#   #                                   "D SAM" = "Decarboxylated S Adenosylmethionine",
+#   #                                   "F6 phosphate" = "Fructose 6 phosphate",
+#   #                                   "Keto    methylthio butyric acid" = "Keto methylthiobutyric acid",
+#   #                                   "N e  Acetyl Lysine" = "Ne Acetyl lysine")) %>%
+#   mutate_at(c("Compound.Name_SQL"), replace_double_spaces)
+
+LauraEditsSQL <- read.csv("data_extra/Standards_LeadingNumbers_LTC.csv") %>%
   select(Compound.Name_SQL, Compound.Name_SQL_LTC)
   
 Last.Fixes <- Ingalls_Lab_Standards_Classyfire %>%
