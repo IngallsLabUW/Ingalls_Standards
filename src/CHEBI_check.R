606a678364ab0c0e1fce8c1b1cb8efd1b777fa0a
Regina Lionheart <regina@lionhe.art>
2021-06-22 10:49:14 -0700

    Tidy Chebi.

--- src/CHEBI_check.R
+++ src/CHEBI_check.R
@@ -1,9 +1,7 @@
-library(httr) #For requesting info from the internet
-library(dplyr) # For piping and data shaping
-
 # Define a function that accepts a KEGG ID (format cpd:C[number]) and returns
 # the ChEBI id found on the KEGG webpage. If there are multiple, return the
 # first one. If not found, return NA.
+
 grabChEBI <- function(compound_id){
   if(is.na(compound_id)) {
     return(NA)
@@ -25,31 +23,22 @@ grabChEBI <- function(compound_id){
 }
 
 # Reassign the FigNames standards
-latest.standards <- Ingalls_Lab_Standards_FigNames
+All_Standards <- Ingalls_Lab_Standards_FigNames
 
 # Apply the function defined above to each ChEBI id
-new_ChEBI <- sapply(unique(latest.standards$C0), grabChEBI)
+new_ChEBI <- sapply(unique(All_Standards$C0), grabChEBI)
 # Replace the item with the obnoxious name <NA> with the character "NA"
 names(new_ChEBI)[which(is.na(names(new_ChEBI)))] <- "NA"
 
 # Merge with standards for comparison
 ChEBI_comparison <- new_ChEBI %>%
   data.frame(C0=names(.), new_ChEBI=paste0("CHEBI:", .)) %>%
-  left_join(latest.standards, ., by="C0")
+  left_join(All_Standards, by="C0")
 
-# Select just the relevant columns for comparison
-ChEBI_comparison <- ChEBI_comparison %>%
-  select(Compound.Name, C0, new_ChEBI, CHEBI)
-
-# Investigate the items that differ
-ChEBI_comparison_diff <- ChEBI_comparison %>%
-  select(Compound.Name, C0, new_ChEBI, CHEBI) %>%
-  filter(new_ChEBI!=CHEBI)
-
-Ingalls_Lab_Standards_ChEBI <- ChEBI_comparison_diff %>%
+# Add to full standards
+Ingalls_Lab_Standards_ChEBI <- ChEBI_comparison %>%
   full_join(Ingalls_Lab_Standards_FigNames) %>%
   unique() %>%
-  rename(ChEBI = new_ChEBI,
-         ChEBI_old = CHEBI) %>%
+  rename(ChEBI = new_ChEBI) %>%
   select(Compound.Type, Column, Compound.Name, Compound.Name_old, Compound.Name_figure,
-         QE.LinRange:z, C0, ChEBI, ChEBI_old, Fraction1:KEGGNAME)
+         QE.LinRange:z, C0, ChEBI, Fraction1:KEGGNAME)
