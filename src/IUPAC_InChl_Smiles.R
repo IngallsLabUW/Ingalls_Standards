bc3118f144109a0047ad3a1df32d370879f59314
Regina Lionheart <regina@lionhe.art>
2021-07-06 16:20:01 -0700

    First restful api for all alt names.

--- src/IUPAC_InChl_Smiles.R
+++ src/IUPAC_InChl_Smiles.R
@@ -1,5 +1,14 @@
 ## Incorporate IUPAC, InChl, and SMILES keys into Ingalls Standards csv.
 
+# could modify the path with a loop using the chemical formulas
+
+path <- "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/1,2,3,4,5/property/MolecularFormula,CanonicalSMILES/CSV"
+path <- "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/Acetylcarnitine/property/MolecularFormula,CanonicalSMILES/CSV"
+
+r <- GET(url = path) 
+status_code(r)
+content(r)
+
 All_Standards <- Ingalls_Lab_Standards_ChEBI
 
 IUPAC.InChl.SMILES <- read.csv("data_extra/Ingalls_Lab_Standards_Alt_Names.csv") %>%
