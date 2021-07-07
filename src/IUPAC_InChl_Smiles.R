04a6edb60524f0b02b52b4410939fd43962ace52
Regina Lionheart <regina@lionhe.art>
2021-07-07 11:06:01 -0700

    First full alt names dataframe produced.

--- src/IUPAC_InChl_Smiles.R
+++ src/IUPAC_InChl_Smiles.R
@@ -1,13 +1,55 @@
 ## Incorporate IUPAC, InChl, and SMILES keys into Ingalls Standards csv.
-
+library(httr)
+library(tidyverse)
 # could modify the path with a loop using the chemical formulas
 
-path <- "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/1,2,3,4,5/property/MolecularFormula,CanonicalSMILES/CSV"
-path <- "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/Acetylcarnitine/property/MolecularFormula,CanonicalSMILES/CSV"
+
+standards <- read.csv("Ingalls_Lab_Standards.csv") %>%
+  select(Compound.Name) %>%
+  unique %>%
+  slice(1:3)
+standards <- standards[["Compound.Name"]]
+
+for (i in standards) {
+  path <- paste("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/", i, "/property/Title,MolecularFormula,inchikey,CanonicalSMILES,IUPACName/CSV", sep = "")
+  print(path)
+  
+  r <- GET(url = path) 
+  status_code(r)
+  content(r)
+  mydata <- read.csv(text = gsub("\t\n", "", r), sep = ",",
+                     header = TRUE)
+}
+
+httr::set_config(httr::config(http_version = 0))
+datalist = list()
+
+for (i in standards) {
+  path <- paste("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/", i, "/property/Title,MolecularFormula,inchikey,CanonicalSMILES,IUPACName/CSV", sep = "")
+  print(path)
+  
+  r <- GET(url = path) 
+  status_code(r)
+  content(r)
+  dat <- read.csv(text = gsub("\t\n", "", r), sep = ",",
+                     header = TRUE)
+  datalist[[i]] <- dat # add it to your list
+}
+
+big_data = do.call(rbind, datalist)
+
+compound.name <- "L-Methionine"
+
+path <- paste("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/", compound.name, "/property/Title,MolecularFormula,inchikey,CanonicalSMILES,IUPACName/CSV", sep = "")
 
 r <- GET(url = path) 
 status_code(r)
 content(r)
+mydata <- read.csv(text = gsub("\t\n", "", r), sep = ",",
+              header = TRUE)
+
+
+
 
 All_Standards <- Ingalls_Lab_Standards_ChEBI
 
