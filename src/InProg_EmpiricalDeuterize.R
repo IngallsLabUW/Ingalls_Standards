7c1811de25ce48ccbd29a6cc3043e3f35d9e54cc
Regina Lionheart <regina@lionhe.art>
2021-11-04 16:45:43 -0700

    Resolve internal stanard deuterium, issue #22.

--- src/InProg_EmpiricalDeuterize.R
+++ src/InProg_EmpiricalDeuterize.R
@@ -1,35 +1,16 @@
+library(tidyverse)
 
-## Save original empirical formula columns to the "archive data" sheet. 
-Auxilary_Columns <- read.csv("data_old/Ingalls_Lab_Standards_AuxColumns.csv") 
+## Load latest standard version
+Ingalls_Lab_Standards <- read.csv("Ingalls_Lab_Standards.csv")
+
+NonDeuterized <- Ingalls_Lab_Standards %>%
+  mutate(Empirical_Formula = ifelse(Compound_Type == "Internal Standard", 
+                                    gsub("D", "H(2)", Empirical_Formula),
+                                    Empirical_Formula))
 
 
 
-## New column order 
-# Dropped all "QE" and "TQS" columns like RSD, LinRange, Ratio.
-Ingalls_Lab_Standards_ColumnSelection <- Ingalls_Lab_Standards_DropRows %>%
-  select(Compound.Name, m.z, RT..min., z, Column, HILICMix,
-         Conc..uM, ionization_form, Liquid.Fraction, 
-         Compound.Type, Emperical.Formula, PubChem_Formula, 
-         PubChem_Code, KEGG_Code, KEGG_Names, ChEBI_Code,
-         IUPAC_Code, InChI_Key_Code, Canon_SMILES_Code, Classyfire,
-         Compound.Name_old, Compound.Name_figure, Compound.Name_SQL,
-         Date.added, Priority)
 
-## Rename columns! 
-# Note the renamed "old" to "original".
-Ingalls_Lab_Standards_RenamedColumns <- Ingalls_Lab_Standards_ColumnSelection %>%
-  rename(Compound_Name = Compound.Name,
-         mz = m.z,
-         RT_minute = RT..min.,
-         HILIC_Mix = HILICMix,
-         Concentration_uM = Conc..uM,
-         Ionization_Form = ionization_form,
-         Liquid_Fraction = Liquid.Fraction,
-         Compound_Type = Compound.Type,
-         Empirical_Formula = Emperical.Formula,
-         Compound_Name_Original = Compound.Name_old,
-         Compound_Name_Figure = Compound.Name_figure,
-         Compound_Name_SQL = Compound.Name_SQL,
-         Date_Added = Date.added)
+
 
 
