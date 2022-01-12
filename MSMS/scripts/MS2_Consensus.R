b0997b778bfc925693f96ffe9753fd15cc3886f7
Regina Lionheart <regina@lionhe.art>
2022-01-11 16:18:56 -0800

    Small comments on MS2_Consensus.R

--- MSMS/scripts/MS2_Consensus.R
+++ MSMS/scripts/MS2_Consensus.R
@@ -1,35 +1,19 @@
 library(tidyverse)
 
-## Will's code: Small edits made by Regina
+## Will's code: Edits made by Regina
 
-# RML function for scaling raw MS2 data
-ScaleMS2 <- function(scan) {
-  scantable <- read.table(text = as.character(scan),
-                          col.names = c("mz", "intensity"), fill = TRUE) %>%
-    mutate(mz = as.numeric(mz %>% str_replace(",", "")),
-           intensity = as.numeric(intensity %>% str_replace(";", "")),
-           intensity = round(intensity / max(intensity) * 100, digits = 1)) %>%
-    filter(intensity > 0.5) %>%
-    arrange(desc(intensity)) %>%
-    unite("MS2", mz:intensity, sep = ", ") %>%
-    mutate(MS2 = paste(MS2, collapse = "; ")) %>% # This is adding the ".MS2 addition on the 
-    unique()
-  
-  return(scantable)
+# WKumler function to assign intensity subgroups within fragment groups
+assign_subgroup <- function(mzs, ints, filenames){
+  data_mat <- cbind(mzs, ints)
+  n_centers <- ceiling(nrow(data_mat)/length(unique(filenames)))
+  if(n_centers==nrow(data_mat)){
+    seq_len(n_centers)
+  } else {
+    kmeans(data_mat, centers = n_centers)$cluster
+  }
 }
 
-# Import complete five-run MSMS data
-Ingalls_Lab_Standards_MSMS <- read.csv("MSMS/data_processed/Ingalls_Lab_Standards_MSMS.csv")
-
-# Rep
-ScaledforMS2 <- Ingalls_Lab_Standards_MSMS %>%
-  drop_na() %>%
-  rowwise() %>%
-  mutate(ScaleMS2(MS2))
-
-## Will's grouping function
-## IS THIS BASED ON M/Z AND INTENSITY? Ie what about thymidine voltage 35 group 1?
-## Are you assigning group 1 based on size and intensity?
+# WKumler function to assign fragment group based on m/z
 mz_group <- function(mz_vals, ppm) {
   group_vec <- numeric(length(mz_vals))
   group_num <- 1L
@@ -46,16 +30,39 @@ mz_group <- function(mz_vals, ppm) {
   group_vec
 }
 
-assign_subgroup <- function(mzs, ints, filenames){
-  data_mat <- cbind(mzs, ints)
-  n_centers <- ceiling(nrow(data_mat)/length(unique(filenames)))
-  if(n_centers==nrow(data_mat)){
-    seq_len(n_centers)
-  } else {
-    kmeans(data_mat, centers = n_centers)$cluster
-  }
+# RML function for scaling raw MS2 intensity data
+ScaleMS2Intensity <- function(scan) {
+  scantable <- read.table(text = as.character(scan),
+                          col.names = c("mz", "intensity"), fill = TRUE) %>%
+    mutate(mz = as.numeric(mz %>% str_replace(",", "")),
+           intensity = as.numeric(intensity %>% str_replace(";", "")),
+           intensity = round(intensity / max(intensity) * 100, digits = 1)) %>%
+    filter(intensity > 0.5) %>%
+    arrange(desc(intensity)) %>%
+    unite("MS2", mz:intensity, sep = ", ") %>%
+    mutate(MS2 = paste(MS2, collapse = "; ")) %>% # This is adding the ".MS2 addition on the 
+    unique()
+  
+  return(scantable)
 }
 
+
+# Import complete five-run MSMS data and original standards list
+Ingalls_Lab_Standards <- read.csv("Ingalls_Lab_Standards.csv") %>%
+  select(compound_name = Compound_Name, HILIC_Mix) %>%
+  unique()
+Ingalls_Lab_Standards_MSMS <- read.csv("MSMS/data_processed/Ingalls_Lab_Standards_MSMS.csv") %>%
+  left_join(Ingalls_Lab_Standards, by = "compound_name") %>%
+  filter(str_detect(filename, HILIC_Mix)) %>%
+  select(-HILIC_Mix)
+
+# Scale the intensity to 100 and filter out intensities < 0.5
+ScaledforMS2 <- Ingalls_Lab_Standards_MSMS %>%
+  drop_na() %>%
+  rowwise() %>%
+  mutate(ScaleMS2Intensity(MS2))
+
+# Separate, arrange MS2 data, assign fragment group
 #msdata <- read.csv("https://raw.githubusercontent.com/IngallsLabUW/Ingalls_Standards/dbbee62372e93ff9f5992ce083f0509465f94308/MSMS/data_processed/Ingalls_Lab_Standards_MSMS.csv") %>%
 msdata_pre <- ScaledforMS2 %>%
   separate_rows(MS2, sep = "; ") %>%
@@ -67,13 +74,20 @@ msdata_pre <- ScaledforMS2 %>%
   # Create the groups within compound_name and voltage
   mutate(frag_group = mz_group(mz, ppm = 5)) 
 
-v <- msdata_pre %>%
-  filter(str_detect(filename, "Mix1")) %>%
-  filter(compound_name=="Glycine betaine") %>%
+subgroup_test <- msdata_pre %>%
+  filter(voltage==20) %>% # & compound_name=="Trehalose" & 
   group_by(voltage, frag_group) %>%
   # filter(voltage==20, frag_group==1) %>%
   mutate(sub_group=assign_subgroup(mz, int, filename))
 
+# Visualize
+subgroup_test %>% filter(voltage==20) %>% ggplot() + 
+  geom_point(aes(x=mz, y=int, color=filename)) + facet_wrap(~frag_group, scales = "free")
+subgroup_test %>% filter(voltage==20) %>% ggplot() + 
+  geom_point(aes(x=mz, y=int, color=factor(sub_group))) + facet_wrap(~frag_group, scales = "free")
+
+
+# "Finalize" with filtering
 msdata_final <- msdata_pre %>%
   group_by(frag_group, .add = TRUE) %>%
   # Do any filtering you want here
@@ -86,14 +100,5 @@ msdata_final <- msdata_pre %>%
   mutate(n=n()) %>%
   filter(n>=2) %>%
   # Take the median of the mz and intensity
-  summarise(mz=median(mz), int=median(int)) %>% # originally mean
+  summarise(mz=median(mz), int=median(int)) %>%
   summarise(MS2=paste(mz, int, sep = ", ", collapse = "; "))
-
-variance_test <- msdata_pre %>%
-  ungroup() %>% 
-  group_by(voltage, compound_name, filename) %>%
-  mutate(variance_mz = var(mz)) %>%
-  mutate(variance_int = var(int)) %>%
-  select(voltage, compound_name, filename, variance_mz, variance_int) %>%
-  unique()
-
