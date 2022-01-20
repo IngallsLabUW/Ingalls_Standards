45ac59906390ed24d1f62e783c7b04b09131e6e9
Regina Lionheart <regina@lionhe.art>
2022-01-20 12:07:14 -0800

    First successful MS2 cosine similarity calculation.

--- TestSimScript.R
+++ TestSimScript.R
@@ -46,20 +46,36 @@ ScaleMS2Intensity <- function(scan) {
 }
 
 # RML/KRH function for finding cosine similarity
-MS2CosineSimilarity <- function(scan1, scan2) {
+MS2CosineSimilarity <- function(scan1, scan2, mz.flexibility) {
   
-  weight1 <- (scan1[, 1] ^ 2) * sqrt(scan1[, 2])
-  weight2 <- (scan2[, 1] ^ 2) * sqrt(scan2[, 2])
+  mz.flexibility <- mz.flexibility
+  scan1 <- MakeScantable(scan1)
+  scan2 <- MakeScantable(scan2)
   
-  diff.matrix <- sapply(scan1[, 1], function(x) scan2[,1] - x)
+  weight1 <- (scan1[, "mz"] ^ 2) * sqrt(scan1[, "intensity"])
+  weight2 <- (scan2[, "mz"] ^ 2) * sqrt(scan2[, "intensity"])
   
-  same.index <- which(abs(diff.matrix[[1]]) < 0.02, arr.ind = TRUE)
+  diff.matrix <- sapply(scan1[, "mz"], function(x) scan2[, "mz"] - x)
+  same.index <- which(abs(diff.matrix) < 0.02, arr.ind = TRUE)
   cosine.similarity <- sum(weight1[same.index[, 2]] * weight2[same.index[, 1]]) /
     (sqrt(sum(weight2 ^ 2)) * sqrt(sum(weight1 ^ 2)))
   
   return(cosine.similarity)
 }
 
+MakeScantable <- function(concatenated.scan) {
+  requireNamespace("dplyr", quietly = TRUE)
+  
+  scantable <- read.table(text = as.character(concatenated.scan),
+                          col.names = c("mz", "intensity"), fill = TRUE) %>%
+    dplyr::mutate(mz = as.numeric(mz %>% stringr::str_replace(",", "")),
+                  intensity = as.numeric(intensity %>% stringr::str_replace(";", "")),
+                  intensity = round(intensity / max(intensity) * 100, digits = 1)) %>%
+    dplyr::filter(intensity > 0.5) %>%
+    dplyr::arrange(desc(intensity))
+  
+  return(scantable)
+}
 
 # Import complete five-run MSMS data and original standards list
 # Scale intensity to 100 and filter out intensities < 0.5
@@ -92,43 +108,41 @@ intensity_subgroups <- msdata_frags %>%
   group_by(frag_group, compound_name, voltage) %>% 
   mutate(sub_group=assign_subgroup(mz, int, filename))
 
-## Option 2 test run
-option2 <- intensity_subgroups %>%
+## Massage into appropriate form 
+algorithm2_df <- intensity_subgroups %>%
   group_by(compound_name, voltage, frag_group, filename) %>%
   top_n(1, int) %>%
   slice_sample() %>%
   filter(compound_name == "Gonyol" & voltage == 20)
 
-scan1 <- option2 %>%
+dataframe_firstfour <- algorithm2_df %>%
   filter(!str_detect(filename, "pos5")) %>%
   group_by(frag_group) %>%
   mutate(mz = median(mz)) %>%
   mutate(int = median(int)) %>%
   ungroup() %>%
-  select(mz, int) %>%
+  unite(MS2, c(mz, int), sep = ", ") %>%
+  select(-filename, -frag_group, -sub_group) %>%
+  unique() %>%
+  mutate(MS2_firstfour = str_c(MS2, collapse = "; ")) %>%
+  select(-MS2) %>%
   unique()
 
-scan2 <- option2 %>%
+dataframe_lastone <- algorithm2_df %>%
   filter(str_detect(filename, "pos5")) %>%
   ungroup() %>%
-  select(mz, int)
+  unite(MS2, c(mz, int), sep = ", ") %>%
+  select(-filename, -frag_group, -sub_group) %>%
+  mutate(MS2_lastone = str_c(MS2, collapse = "; ")) %>%
+  select(-MS2) %>%
+  unique()
 
-## Find the cosine similarity of the other files to file 5
 
+########################################################################
 
-# ------------------------
-# "Finalize" with filtering
-msdata_final <- msdata_frags %>%
-  group_by(frag_group, .add = TRUE) %>%
-  # Do any filtering you want here
-  # E.g. choose only the largest fragment from each fragment group per file
-  group_by(filename, .add = TRUE) %>%
-  slice(1) %>%
-  ungroup(filename) %>%
-  # E.g. only include fragments that appear in more than one file
-  # Could be increased with more files!
-  mutate(n=n()) %>%
-  filter(n>=2) %>%
-  # Take the median of the mz and intensity
-  summarise(mz=median(mz), int=median(int)) %>%
-  summarise(MS2=paste(mz, int, sep = ", ", collapse = "; "))
+# Tidy and add required columns to input data
+
+test <- dataframe_firstfour %>%
+  left_join(dataframe_lastone) %>%
+  rowwise() %>%
+  dplyr::mutate(MS2_cosine_similarity1 = MS2CosineSimilarity(MS2_firstfour, MS2_lastone))
