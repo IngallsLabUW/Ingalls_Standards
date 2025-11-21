0444146d8dbcd0ea16d5b2257c766631c8a98f06
wkumler <wkumler@uw.edu>
2025-11-21 10:32:48 -0800

    Fixing TMAB polarity

--- MSMS/mgf_maker.R
+++ MSMS/mgf_maker.R
@@ -2,24 +2,22 @@
 library(tidyverse)
 
 stans_fixed <- read_csv("MSMS/stans_fixed.csv") %>%
-  mutate(polarity=list(ifelse(polarity=="both", list(c("pos", "neg")), list(polarity))), .by=compound_name) %>%
-  unnest(polarity) %>%
+  mutate(polarity = map(polarity, function(x) if (x == "both"){c("pos","neg")}else{x}),
+         .by = compound_name) %>%
   unnest(polarity)
-best_frags <- read_csv("MSMS/best_frags.csv")
 
-stans_fixed %>%
-  filter(compound_name=="Guanine") %>%
-  left_join(best_frags) %>%
-  head()
+best_frags <- read_csv("MSMS/best_frags.csv")
 
 stans_fixed %>%
   left_join(best_frags) %>%
   arrange(compound_name, polarity, first_scan, fragmz) %>%
-  mutate(scan_id = paste0(compound_name, "_", polarity),
-         .by = c(compound_name, polarity)) %>%
   reframe(text = c("BEGIN IONS",
-                   paste0("TITLE=", unique(scan_id)),
-                   paste0("PEPMASS=", unique(mz)),
+                   paste0("TITLE=", compound_name),
+                   paste0("PEPMASS=", mz),
+                   paste0("CHARGE=", case_when(
+                     polarity[1] == "pos" ~ "1+",
+                     polarity[1] == "neg" ~ "1-"
+                   )),
                    paste0(fragmz, " ", int),
                    "END IONS"),
           .by = c(compound_name, polarity)) %>%
