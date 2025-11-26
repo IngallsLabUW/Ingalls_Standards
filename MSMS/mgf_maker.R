62fddeb3295073fec1929400ed62ec04534117a3
wkumler <wkumler@uw.edu>
2025-11-26 11:33:22 -0800

    Adding an MSP version of the library as well

--- MSMS/mgf_maker.R
+++ MSMS/mgf_maker.R
@@ -13,15 +13,62 @@ stans_fixed %>%
   arrange(compound_name, polarity, first_scan, fragmz) %>%
   reframe(text = c("BEGIN IONS",
                    paste0("TITLE=", unique(compound_name)),
+                   paste0("NAME=",  unique(compound_name)),
+                   paste0("RTINSECONDS=",unique((rtmin+rtmax)/2*60)),
                    paste0("PEPMASS=", unique(mz)),
                    paste0("CHARGE=", case_when(
                      polarity[1] == "pos" ~ "1+",
                      polarity[1] == "neg" ~ "1-"
                    )),
-                   paste0("Num peaks=", n()),
                    paste0(fragmz, " ", int),
                    "END IONS"),
           .by = c(compound_name, polarity)) %>%
   # print(n=40)
   pull(text) %>%
   writeLines("MSMS/ingalls_library_70ce.mgf")
+
+
+
+stans_fixed %>%
+  left_join(best_frags) %>%
+  arrange(compound_name, polarity, first_scan, fragmz) %>%
+  reframe(text = c("BEGIN IONS",
+                   paste0("TITLE=", unique(compound_name)),
+                   paste0("NAME=",  unique(compound_name)),
+                   paste0("RTINSECONDS=",unique((rtmin+rtmax)/2*60)),
+                   paste0("PEPMASS=", unique(mz)),
+                   paste0("CHARGE=", case_when(
+                     polarity[1] == "pos" ~ "1+",
+                     polarity[1] == "neg" ~ "1-"
+                   )),
+                   paste0(fragmz, " ", int),
+                   "END IONS"),
+          .by = c(compound_name, polarity)) %>%
+  # print(n=40)
+  pull(text) %>%
+  writeLines("MSMS/ingalls_library_70ce.mgf")
+
+
+
+stans_fixed %>%
+  left_join(best_frags) %>%
+  arrange(compound_name, polarity, first_scan, fragmz) %>%
+  reframe(text = c(
+    paste0("Name: ", unique(compound_name)),
+    paste0("Precursor_type: ", case_when(
+      polarity[1] == "pos" ~ "[M+H]+",
+      polarity[1] == "neg" ~ "[M-H]-"
+    )),
+    paste0("Ion_mode: ", case_when(
+      polarity[1] == "pos" ~ "P",
+      polarity[1] == "neg" ~ "N"
+    )),
+    paste0("PrecursorMZ: ", unique(mz)),
+    paste0("Retention_time: ", unique((rtmin+rtmax)/2*60)),
+    paste0("Num Peaks: ", length(fragmz)),
+    paste(fragmz, int)
+  ),
+  .by = c(compound_name, polarity)) %>%
+  # print(n=40)
+  pull(text) %>%
+  writeLines("MSMS/ingalls_library_70ce.msp")
